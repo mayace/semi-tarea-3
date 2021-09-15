c6c083011b17b352cc520df2957e70c5f6eab1c8
ctejaxun <cesar.etx@gmail.com>
2021-09-15 10:37:42 -0600

    boto3 - configura boto3 con rekognition * agrega ruta para leer imagen en base64 * enviar imagen a rekognition para obtener etiquetas (labels) * uso de .env para credenciales aws

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from typing import Optional
-
 from fastapi import FastAPI
 
+from pydantic import BaseModel, BaseSettings
+import boto3
+import base64
+
+class Settings(BaseSettings):
+    rekognition_id =""
+    rekognition_secret=""
+    rekognition_region="us-east-1"
+    class Config:
+            env_file = ".env"
+class Body(BaseModel):
+    image: str
+
+    
 
 app = FastAPI()
+settings= Settings()
 
-@app.get("/")
-def get_tags():
-    return "Dude"
+@app.post("/tarea3-200819117")
+def get_tags(body: Body):
+
+    options = dict(aws_access_key_id =settings.rekognition_id,aws_secret_access_key =settings.rekognition_secret,region_name=settings.rekognition_region, )
+
+    client = boto3.client("rekognition", **options )
+    image = base64.b64decode(body.image.partition(",")[2])
+    response = client.detect_labels(Image=dict(Bytes=image))
+    return response
